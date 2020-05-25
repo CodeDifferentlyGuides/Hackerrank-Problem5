54dddc6bd74edeccab5f74cff2395bbbd1899a99
CodeDifferentlyGuides <65664324+CodeDifferentlyGuides@users.noreply.github.com>
2020-05-25 01:50:38 -0400

    Created SolutionTest.java

---
+++ test/SolutionTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void contructSolutionTest(){
+        int input = "100
+        String expected = "Good job";
+        String actual = "*insert main function call*";
+
+        Assert.assertEquals(expected, actual);
+    }
+}
